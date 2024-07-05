196d2fc6092aa184731b86e3b27f3698c892bd5c
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-07-05 16:46:41 +0530

    feature:Adding cover search

--- apps/web/app/(dashboard)/take-register/page.tsx
+++ apps/web/app/(dashboard)/take-register/page.tsx
@@ -11,6 +11,136 @@ const client = new PrismaClient()
 const today = new Date();
 const session = await getServerSession(authOptions)
 
+
+//get all registers for all users
+async function getListOfALLRegisters() {
+
+  console.log("session---" + JSON.stringify(session))
+  try {
+    const response = await client.register.findMany({
+      where: {
+        date: {
+          gte: new Date(today.getFullYear(), today.getMonth(), today.getDate()), // Greater than or equal to today
+          lt: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1) // Less than tomorrow
+        }
+
+      },
+      include: {
+        cls: {
+          select: {
+            name: true
+          }
+        },
+        teacher: {
+          select: {
+            user: {
+              select: {
+                username: true,
+
+              }
+            }
+          }
+        }
+
+      }
+    })
+    console.log("response size--->" + response.length);
+    if (response.length === 0) {
+      console.log("resonse is empty")
+      const response = await createTodaysAllRegister()
+
+      return response;
+    }
+    return response
+  } catch (error) {
+    console.log(error)
+    return [];
+  }
+}
+
+async function createTodaysAllRegister() {
+  const classes = await client.cls.findMany()
+  classes.map(async (cls) => {
+
+
+    if (cls) {
+      const register = await client.register.create({
+        data: {
+          classId: cls?.id,
+          teacherId: cls?.teacherId || cls?.id,
+          date: new Date()
+        }
+      });
+
+
+      // Get students for the class
+      const studentsForClass = await client.student.findMany({
+        where: {
+          classId: cls.id
+        }
+      });
+      // Log the students and register details for debugging
+
+      // Create attendance records for students in the class
+      if (studentsForClass) {
+        const attendanceData = studentsForClass.map(student => ({
+          studentId: student.id,
+          registerId: register.id,
+          status: ""
+        }));
+        await client.attendance.createMany({ data: attendanceData });
+        await client.attendance.findMany();
+
+        try {
+          const response = await client.register.findMany({
+            where: {
+              date: {
+                gte: new Date(today.getFullYear(), today.getMonth(), today.getDate()), // Greater than or equal to today
+                lt: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1) // Less than tomorrow
+              }
+            },
+            include: {
+              cls: {
+                select: {
+                  name: true
+                }
+              },
+              teacher: {
+                select: {
+                  user: {
+                    select: {
+                      username: true,
+
+                    }
+                  }
+                }
+              }
+
+            }
+          })
+          console.log("------------------returning create register---------------")
+          if (response.length === 0) {
+            console.log("Inside resonse is empty")
+          }
+          return response;
+        } catch (error) {
+          console.log(error)
+          return [];
+        }
+
+      }
+
+    }
+
+  })
+
+}
+
+
+//creat all registers for all users
+
+
+// get todays register for user
 async function getListOfRegisters() {
 
   console.log("session---" + JSON.stringify(session))
@@ -47,7 +177,7 @@ async function getListOfRegisters() {
     })
     if (response.length === 0) {
       console.log("resonse is empty")
-      const response =await createTodaysRegister ()
+      const response = await createTodaysRegister()
 
       return response;
     }
@@ -58,7 +188,8 @@ async function getListOfRegisters() {
   }
 }
 
-async function createTodaysRegister () {
+//create todays register for user
+async function createTodaysRegister() {
   const cls = await client.cls.findFirst({
     where: {
       teacherId: Number(session.user.id)
@@ -69,7 +200,7 @@ async function createTodaysRegister () {
     const register = await client.register.create({
       data: {
         classId: cls?.id,
-        teacherId: Number(session.user.id),
+        teacherId: cls?.teacherId || cls?.id,
         date: new Date()
       }
     });
@@ -141,7 +272,15 @@ async function createTodaysRegister () {
 
 const page = async () => {
 
-  const regData: any = await getListOfRegisters();
+  const regData: any = await getListOfALLRegisters();
+  console.log("Regdata----" + JSON.stringify(regData));
+  const usersRegister = regData.filter((register: any) => {
+    if (register.teacherId === Number(session.user.id)) {
+      return register
+    }
+
+
+  })
 
   return (
 
@@ -161,7 +300,7 @@ const page = async () => {
       <div className='text-2xl font-bold tracking-tight '>Take Register</div>
       <div className='flex space-x-4'>
 
-        <ListRegisterClient regData={regData} />
+        <ListRegisterClient regData={regData} register={usersRegister} />
 
       </div>
 
--- apps/web/components/clients/ListRegisterClient.tsx
+++ apps/web/components/clients/ListRegisterClient.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@repo/ui/"
-import { Tag } from "@repo/ui/"
+import { Button, Card, CardContent, CardDescription, CardHeader, CardTitle, Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, Separator } from "@repo/ui/"
 import { Badge } from "@repo/ui/"
-
+import { CalendarIcon } from "lucide-react"
+import { Popover, PopoverContent, PopoverTrigger } from "@repo/ui/"
 import Link from "next/link"
+import { useState } from "react"
 
 
 
@@ -41,28 +42,71 @@ export interface registerType {
 
 
 
-const ListRegisterClient = ({ regData }: { regData: registerType[] }) => {
+const ListRegisterClient = ({ regData, register }: { regData: registerType[], register: registerType[] }) => {
 
-
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState("")
   console.log(regData)
   return (
-    <div className="w-full space-y-4">
-      {regData ? (regData.map(r => <Link className="w-full" href={{
-        pathname: `./take-register/register/${r.id}`,
+    <div className="flex flex-col w-full space-y-8">
+      <div className="flex w-full justify-between">
+        <div className="">
+          <Popover  >
+            <PopoverTrigger className="w-72">
+              <Command className="rounded-lg border  w-72">
+                <CommandInput placeholder="Search classes or registers..." className="w-72" />
+              </Command>
 
-      }}><div className="w-full pb-4"><Card className="flex items-center justify-between">
-        <CardHeader>
-          <CardTitle>{r.cls.name} {r.id}</CardTitle>
-          <CardDescription>{r.teacher.user.username}</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col items-center">
-          <Badge className={`${r.status === "Completed" ? "bg-green-300 text-green-800" : "bg-gray-200 text-gray-800"} p-1 px-2 rounded-2xl `}>{r.status !==""?r.status:"Ready"}</Badge>
-        </CardContent>
+            </PopoverTrigger>
+            <PopoverContent className="max-w-full">
+              <Command className="min-w-full">
+                <CommandList className="min-w-full" >
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup className="min-w-full" heading="List of registers">
+                    {regData.map((register) =>
+                      <Link className="w-full" href={{
+                        pathname: `./take-register/register/${register.id}`,
 
-      </Card></div></Link>)) : (
-        <div>No register data available.</div>
-      )}
+                      }}><CommandItem className="min-w-full" >
+                          <div className=" text-left">
+                            <span className="px-8 text-left">{"Class " + register.classId}</span>
+                          </div>
+
+                        </CommandItem>
+                      </Link>
+
+                    )}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+
+        </div>
+        <div>
+                    <Button></Button>
+        </div>           
+      </div>
+
+      <div className="w-full space-y-4">
+        {register ? (register.map(r => <Link className="w-full" href={{
+          pathname: `./take-register/register/${r.id}`,
+
+        }}><div className="w-full pb-4"><Card className="flex items-center justify-between">
+          <CardHeader>
+            <CardTitle>{r.cls.name}</CardTitle>
+            <CardDescription>{r.teacher.user.username}</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center">
+            <Badge className={`${r.status === "Completed" ? "bg-green-300 text-green-800" : "bg-gray-200 text-gray-800"} p-1 px-2 rounded-2xl `}>{r.status !== "" ? r.status : "Ready"}</Badge>
+          </CardContent>
+
+        </Card></div></Link>)) : (
+          <div>No register data available.</div>
+        )}
+      </div>
     </div>
+
   )
 }
 
