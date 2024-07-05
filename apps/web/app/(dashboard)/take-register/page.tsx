baa0272ad4af75de111b899c25d20b12a08205c2
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-07-05 12:26:08 +0530

    Updated create register logic

--- apps/web/app/(dashboard)/take-register/page.tsx
+++ apps/web/app/(dashboard)/take-register/page.tsx
@@ -9,9 +9,9 @@ import { authOptions } from "../../lib/auth";
 
 const client = new PrismaClient()
 const today = new Date();
+const session = await getServerSession(authOptions)
 
 async function getListOfRegisters() {
-  const session = await getServerSession(authOptions)
 
   console.log("session---" + JSON.stringify(session))
   try {
@@ -47,91 +47,95 @@ async function getListOfRegisters() {
     })
     if (response.length === 0) {
       console.log("resonse is empty")
-      //createTodaysRegister
+      const response =await createTodaysRegister ()
 
-      const cls = await client.cls.findFirst({
-        where: {
-          teacherId: Number(session.user.id)
-        }
-      }
-      )
-      if (cls) {
-        const register = await client.register.create({
-          data: {
-            classId: cls?.id,
-            teacherId: Number(session.user.id),
-            date: new Date()
-          }
-        });
-
-
-        // Get students for the class
-        const studentsForClass = await client.student.findMany({
-          where: {
-            classId: cls.id
-          }
-        });
-        // Log the students and register details for debugging
-
-        // Create attendance records for students in the class
-        if (studentsForClass) {
-          const attendanceData = studentsForClass.map(student => ({
-            studentId: student.id,
-            registerId: register.id,
-            status: ""
-          }));
-          await client.attendance.createMany({ data: attendanceData });
-          await client.attendance.findMany();
-
-          try {
-            const response = await client.register.findMany({
-              where: {
-                date: {
-                  gte: new Date(today.getFullYear(), today.getMonth(), today.getDate()), // Greater than or equal to today
-                  lt: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1) // Less than tomorrow
-                },
-                teacher: {
-                  userId: Number(session.user.id)
-                }
-
-              },
-              include: {
-                cls: {
-                  select: {
-                    name: true
-                  }
-                },
-                teacher: {
-                  select: {
-                    user: {
-                      select: {
-                        username: true,
-
-                      }
-                    }
-                  }
-                }
-
-              }
-            })
-            console.log("------------------returning create register---------------")
-            if (response.length === 0) {
-              console.log("Inside resonse is empty")
-            }
-            return response;
-          } catch (error) {
-            console.log(error)
-            return [];
-          }
-
-        }
-
-      }
+      return response;
     }
     return response
   } catch (error) {
     console.log(error)
     return [];
+  }
+}
+
+async function createTodaysRegister () {
+  const cls = await client.cls.findFirst({
+    where: {
+      teacherId: Number(session.user.id)
+    }
+  }
+  )
+  if (cls) {
+    const register = await client.register.create({
+      data: {
+        classId: cls?.id,
+        teacherId: Number(session.user.id),
+        date: new Date()
+      }
+    });
+
+
+    // Get students for the class
+    const studentsForClass = await client.student.findMany({
+      where: {
+        classId: cls.id
+      }
+    });
+    // Log the students and register details for debugging
+
+    // Create attendance records for students in the class
+    if (studentsForClass) {
+      const attendanceData = studentsForClass.map(student => ({
+        studentId: student.id,
+        registerId: register.id,
+        status: ""
+      }));
+      await client.attendance.createMany({ data: attendanceData });
+      await client.attendance.findMany();
+
+      try {
+        const response = await client.register.findMany({
+          where: {
+            date: {
+              gte: new Date(today.getFullYear(), today.getMonth(), today.getDate()), // Greater than or equal to today
+              lt: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1) // Less than tomorrow
+            },
+            teacher: {
+              userId: Number(session.user.id)
+            }
+
+          },
+          include: {
+            cls: {
+              select: {
+                name: true
+              }
+            },
+            teacher: {
+              select: {
+                user: {
+                  select: {
+                    username: true,
+
+                  }
+                }
+              }
+            }
+
+          }
+        })
+        console.log("------------------returning create register---------------")
+        if (response.length === 0) {
+          console.log("Inside resonse is empty")
+        }
+        return response;
+      } catch (error) {
+        console.log(error)
+        return [];
+      }
+
+    }
+
   }
 }
 
