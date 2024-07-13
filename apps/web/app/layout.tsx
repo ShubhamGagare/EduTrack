2019d71c5b5bc96dcb2dc411d6275d13942daf6b
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-07-13 19:20:51 +0530

    updated metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,8 +7,8 @@ import AppbarClient from "../components/clients/AppbarClient";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wallet",
-  description: "Simple wallet app",
+  title: "Edusys",
+  description: "School information system",
 };
 
 export default function RootLayout({
