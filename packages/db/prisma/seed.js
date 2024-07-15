12410fd403a7274727513a3541c682f73302b191
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-07-15 10:45:39 +0530

    prisma updated

--- packages/db/prisma/seed.js
+++ packages/db/prisma/seed.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('./generated/client');
 const { faker } = require('@faker-js/faker');
 const prisma = new PrismaClient();
 
