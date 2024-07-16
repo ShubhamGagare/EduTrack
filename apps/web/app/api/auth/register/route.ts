6eda8f04b4acf73e408e74d26ce53870d9031297
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-07-16 17:37:54 +0530

    fix:import bcrypt library

--- apps/web/app/api/auth/register/route.ts
+++ apps/web/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { hash } from "bcrypt";
+import { hash } from "bcryptjs";
 import { sql } from "@vercel/postgres";
 import { PrismaClient } from "../../../../../../packages/db/prisma/generated/client";
 
