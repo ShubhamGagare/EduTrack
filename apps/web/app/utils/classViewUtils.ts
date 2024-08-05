539d6fcfac6cd4627046cb24b471bbd6eb6c0c24
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-08-05 16:52:29 +0530

    Remove classUtils

--- apps/web/app/utils/classViewUtils.ts
+++
@@ -1,9 +0,0 @@
-// import { PrismaClient } from "../../../../packages/db/prisma/generated/client";
-// import { authOptions } from "../lib/auth";
-// import { getServerSession } from "next-auth";
-// import axios from "axios";
-// import { NextApiRequest } from "next";
-// import OpenAI from 'openai';
-// import { NextResponse } from "next/server";
-// import { registerType } from "components/clients/ListRegisterClient";
-
