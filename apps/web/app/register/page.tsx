c37a5dd9af917549e040e326c83f775ae234a104
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-08-11 01:29:48 +0530

    Update login and SignUp page styling

--- apps/web/app/register/page.tsx
+++ apps/web/app/register/page.tsx
@@ -15,7 +15,7 @@ export default async function RegisterPage() {
 
     <div className="flcontainer relative  min-w-[300px] h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0ex">
     <a className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 absolute right-4 top-4 md:right-8 md:top-8 border" href="/login" >Login</a>
-    <div className="relative hidden justify-center h-full flex-col bg-blue-100 p-10 pb-10 text-blue-900 dark:border-r lg:flex items-center">
+    <div className="relative hidden justify-center h-full flex-col bg-violet-100 p-10 pb-10 text-violet-900 dark:border-r lg:flex items-center">
         
         <label className="text-8xl font-bold ">EduTrack</label>
         <label className="text-2xl  ">Empowering Education with Smart AI Solutions</label>
