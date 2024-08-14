1b0240101f9ec66df33379f886421476ccbc3204
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-08-14 16:39:29 +0530

    fix:update prisma client

--- apps/web/components/ListOfClasses.tsx
+++ apps/web/components/ListOfClasses.tsx
@@ -1,10 +1,6 @@
-import { Card } from "@repo/ui";
-import axios from "axios";
 import { ListOfRegisters } from "./ListOfRegisters";
-import { PrismaClient } from "../../../packages/db/prisma/generated/client";
 import { getListOfALLRegisters } from "../app/utils/utils";
 
-const client = new PrismaClient()
 export async function ListOfClasses() {
     const response: any = await getListOfALLRegisters(new Date())
 
