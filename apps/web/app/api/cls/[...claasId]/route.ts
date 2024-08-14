318d29c802c19a2ac9bd0a65d0da8db6410b2e16
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-08-14 23:01:14 +0530

    fix:Class and student apis

--- apps/web/app/api/cls/[...claasId]/route.ts
+++ apps/web/app/api/cls/[...claasId]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
-//import { PrismaClient } from "../../../../../../packages/db/prisma/generated/client";
+import { PrismaClient } from "../../../../../../packages/db/prisma/generated/client";
 
-//const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export async function GET(req:NextRequest) {
     try {
@@ -9,26 +9,26 @@ export async function GET(req:NextRequest) {
         const id = searchParams.get('id');
 
       // Fetch all cls from the database
-    //   const cls = await prisma.student.findUnique({
-    //     where:{
-    //         id:Number(id)
-    //     },
-    //     include:{
-    //       user:{
-    //         select:{
-    //           username:true
-    //         }
-    //       }
-    //     }
-    //   });
+      const cls = await prisma.student.findUnique({
+        where:{
+            id:Number(id)
+        },
+        include:{
+          user:{
+            select:{
+              username:true
+            }
+          }
+        }
+      });
   
       // Send the list of cls as a JSON response
-      return ""//NextResponse.json(cls);
+      return NextResponse.json(cls);
     } catch (error) {
       console.error('Error fetching cls:', error);
       return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     } finally {
       // Disconnect the Prisma Client
-      //await prisma.$disconnect();
+      await prisma.$disconnect();
     }
   }
