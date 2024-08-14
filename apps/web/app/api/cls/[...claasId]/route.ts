7beca4a67419a638b4673d67cada926db5fbf43b
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-08-14 22:58:28 +0530

    Fix:Update class and students api routes

--- apps/web/app/api/cls/[...claasId]/route.ts
+++ apps/web/app/api/cls/[...claasId]/route.ts
@@ -1,34 +1,34 @@
-// import { NextRequest, NextResponse } from 'next/server';
-// import { PrismaClient } from "../../../../../../packages/db/prisma/generated/client";
+import { NextRequest, NextResponse } from 'next/server';
+//import { PrismaClient } from "../../../../../../packages/db/prisma/generated/client";
 
-// const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
-// export async function GET(req:NextRequest) {
-//     try {
-//         const { searchParams } = new URL(req.url);
-//         const id = searchParams.get('id');
+export async function GET(req:NextRequest) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get('id');
 
-//       // Fetch all cls from the database
-//       const cls = await prisma.student.findUnique({
-//         where:{
-//             id:Number(id)
-//         },
-//         include:{
-//           user:{
-//             select:{
-//               username:true
-//             }
-//           }
-//         }
-//       });
+      // Fetch all cls from the database
+    //   const cls = await prisma.student.findUnique({
+    //     where:{
+    //         id:Number(id)
+    //     },
+    //     include:{
+    //       user:{
+    //         select:{
+    //           username:true
+    //         }
+    //       }
+    //     }
+    //   });
   
-//       // Send the list of cls as a JSON response
-//       return NextResponse.json(cls);
-//     } catch (error) {
-//       console.error('Error fetching cls:', error);
-//       return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-//     } finally {
-//       // Disconnect the Prisma Client
-//       await prisma.$disconnect();
-//     }
-//   }
+      // Send the list of cls as a JSON response
+      return ""//NextResponse.json(cls);
+    } catch (error) {
+      console.error('Error fetching cls:', error);
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    } finally {
+      // Disconnect the Prisma Client
+      //await prisma.$disconnect();
+    }
+  }
