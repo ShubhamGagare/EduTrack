a2eb08c547450c5c3da11a98cef7fd747d60f235
Shubham Gagare <shubhamgagare1997@gmail.com>
2024-08-14 22:01:35 +0530

    Prisma binaries updated

--- apps/web/app/api/cls/route.ts
+++ apps/web/app/api/cls/route.ts
@@ -1,20 +1,20 @@
-import { NextResponse } from 'next/server';
-import { PrismaClient } from "../../../../../packages/db/prisma/generated/client";
+// import { NextResponse } from 'next/server';
+// import { PrismaClient } from "../../../../../packages/db/prisma/generated/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function GET() {
-    try {
-      // Fetch all cls from the database
-      const cls = await prisma.cls.findMany();
+// export async function GET() {
+//     try {
+//       // Fetch all cls from the database
+//       const cls = await prisma.cls.findMany();
   
-      // Send the list of cls as a JSON response
-      return NextResponse.json(cls);
-    } catch (error) {
-      console.error('Error fetching cls:', error);
-      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    } finally {
-      // Disconnect the Prisma Client
-      await prisma.$disconnect();
-    }
-  }
+//       // Send the list of cls as a JSON response
+//       return NextResponse.json(cls);
+//     } catch (error) {
+//       console.error('Error fetching cls:', error);
+//       return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+//     } finally {
+//       // Disconnect the Prisma Client
+//       await prisma.$disconnect();
+//     }
+//   }
